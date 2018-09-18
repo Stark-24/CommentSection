e9e725e63f8dda4e1aba797343397b30a69067a7
Alaa Hussein <alaa.hussein401@gmail.com>
2018-09-17 20:07:53 -0700

    complete artistinfo component

--- client/components/ArtistInfo.jsx
+++ client/components/ArtistInfo.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import TracksIcon from "../svg-components/relatedTracks.jsx";
 
 class ArtistInfo extends React.Component {
   constructor(props) {
@@ -21,20 +22,43 @@ class ArtistInfo extends React.Component {
           {/* Artist Stats */}
           <MiniStatsArtist>
             <Followers>
-              <FollowersIcon />
-              <Stats />
+              <img
+                src="https://i.imgur.com/AHNjE1l.png"
+                style={{ width: "16px", height: "12px" }}
+              />
+              <Stats>1,420</Stats>
             </Followers>
-            <Tracks />
-            <FollowButton />
+            <Tracks>
+              <TracksIcon />
+              <Stats>16</Stats>
+            </Tracks>
           </MiniStatsArtist>
+          <FollowButton>Follow</FollowButton>
+          <ReportButton>
+            <img
+              src="https://i.imgur.com/GnG4T0y.png"
+              style={{ width: "15px", height: "14px" }}
+            />
+            <span
+              style={{
+                fontSize: "11px",
+                marginLeft: "5px",
+                color: "#666",
+                marginTop: "1px",
+                position: "absolute"
+              }}
+            >
+              Report
+            </span>
+          </ReportButton>
         </Info>
       </WrapperAboutRight>
     );
   }
 }
-
+const Info = styled.div``;
 const WrapperAboutRight = styled.div`
-  background: red;
+  background: white;
   width: 120px;
   height: 250px;
   margin-top: 20px;
@@ -47,7 +71,10 @@ const ArtistImg = styled.div`
   background-size: cover;
   background-position: 50% 50%;
 `;
-const Info = styled.div``;
+const ReportButton = styled.div`
+  margin-top: 30px;
+  margin-left: 5px;
+`;
 const ArtistName = styled.div`
   width: 120;
   height: 18px;
@@ -60,11 +87,49 @@ const Name = styled.span`
   font-size: 14px;
   color: #333;
 `;
-const MiniStatsArtist = styled.div``;
-const Tracks = styled.div``;
-const FollowButton = styled.div``;
-const Followers = styled.div``;
-const FollowersIcon = styled.div``;
-const Stats = styled.div``;
+const MiniStatsArtist = styled.div`
+  display: flex;
+`;
+const Followers = styled.div`
+  margin-top: 10px;
+  display: block;
+`;
+const Tracks = styled.div`
+  margin-left: 5px;
+  margin-top: 10px;
+  display: block;
+`;
+const FollowButton = styled.div`
+  margin-top: 8px;
+  border: 1px solid #f50;
+  padding-top: 2px;
+  padding-bottom: 2px;
+  padding-left: 8px;
+  padding-right: 9px;
+  width: 34px;
+  height: 16px;
+  background-color: #f50;
+  color: #fff;
+  border-top-left-radius: 3px;
+  border-top-right-radius: 3px;
+  border-bottom-left-radius: 3px;
+  border-bottom-right-radius: 3px;
+  font-family: Interstate, "Lucida Grande", "Lucida Sans Unicode", "Lucida Sans",
+    Garuda, Verdana, Tahoma, sans-serif;
+  font-size: 11px;
+  display: block;
+`;
+const Stats = styled.div`
+  width: auto;
+  height: auto;
+  font-size: 12px;
+  line-height: 16px;
+  font-weight: 100;
+  color: #999;
+  display: inline;
+  margin-top: 1px;
+  margin-left: 3px;
+  position: relative;
+`;
 
 export default ArtistInfo;
---
+++ client/svg-components/relatedTracks.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import styled from "styled-components";
+
+const relatedTracks = props => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 88 88"
+      style={styles.size}
+    >
+      <defs />
+      <g
+        id="Symbol_2_1"
+        data-name="Symbol 2 – 1"
+        transform="translate(-24 -152)"
+      >
+        <rect
+          id="Rectangle_194"
+          data-name="Rectangle 194"
+          style={styles.cls1}
+          width="8"
+          height="18"
+          transform="translate(24 190)"
+        />
+        <rect
+          id="Rectangle_195"
+          data-name="Rectangle 195"
+          styles={styles.cls1}
+          width="8"
+          height="60"
+          transform="translate(44 168)"
+        />
+        <rect
+          id="Rectangle_196"
+          data-name="Rectangle 196"
+          style={styles.cls1}
+          width="8"
+          height="88"
+          transform="translate(64 152)"
+        />
+        <rect
+          id="Rectangle_197"
+          data-name="Rectangle 197"
+          style={styles.cls1}
+          width="8"
+          height="40"
+          transform="translate(84 176)"
+        />
+        <rect
+          id="Rectangle_198"
+          data-name="Rectangle 198"
+          style={styles.cls1}
+          width="8"
+          height="18"
+          transform="translate(104 190)"
+        />
+      </g>
+    </svg>
+  );
+};
+
+const styles = {
+  cls1: {
+    fill: "#333"
+  },
+  size: {
+    width: "16px",
+    height: "12px"
+  }
+};
+
+export default relatedTracks;
